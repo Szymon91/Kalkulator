094ed52666b3e7f738996fcc3eeab5b770f01cad
Szymon91 <Szymon91@poczta.onet.pl>
2019-01-24 12:25:31 +0100

    Poczatek pracy

---
+++ kalkulator.js
@@ -0,0 +1,103 @@
+const numbers = document.querySelectorAll('.number'); //pobranie wszystkich przecisków z cyframi
+const operations = document.querySelectorAll('.operator'); //pobranie wszyskich przecisków funkcyjnych
+
+const buttonPoint = document.querySelector('#point');
+const buttonReset = document.querySelector('#reset');
+const buttonBackspace = document.querySelector('#backspace');
+
+const prevResult = document.querySelector('#previous__result'); //historia wprowadzonych obliczeń
+const result = document.querySelector('#result'); //miejsce wyświetlania wyniku
+
+let value = "0"; //wyświetlana zawartość
+let keepValue; //przechowywana zawartość poprzedniego przycisku;
+let arrayOfValues = []; //przechowywana tablica z pobranymi wartościami ze wszystkich przycisków
+
+let showResult = (num) => {
+    let buttonText = num.target.innerText;
+
+    if (value === "0")
+        value = ''; //jeżeli jest 0 i wcisnę jeden to nie chcę widzieć tego zero
+
+    value += buttonText; //do zmiennej wyświetlającej zawatość jest dodawany tekst z przycisku
+    result.innerText = value; //wyświetlany wynik     
+}
+
+const calculations = (num) => {
+    let operator = num.target.innerText; //przechowujemy tekst z operatora
+
+    switch (operator) {
+        case '+':
+            keepValue = value; //wcześniejsza wartość = wyświetlana wartość
+            prevResult.innerText = value + operator;
+            value = ''; //po kliknięciu wartość znika i przechodzi do historii            
+            result.innerText = value; // aktualizujemy wyświetlaną wartość
+            arrayOfValues.push(keepValue); //dodajemy watość do tablicy
+            arrayOfValues.push('+'); //dodajemy operator do tablicy
+            break;
+        case '-':
+            keepValue = value;
+            prevResult.innerText = value + operator;
+            value = '';           
+            result.innerText = value;
+            arrayOfValues.push(keepValue);
+            arrayOfValues.push('-');
+            break;
+        case 'x':
+            keepValue = value;
+            prevResult.innerText = value + operator;
+            value = '';           
+            result.innerText = value;
+            arrayOfValues.push(keepValue);
+            arrayOfValues.push('*');
+            break;
+        case '/':
+            keepValue = value;
+            prevResult.innerText = value + operator;
+            value = '';          
+            result.innerText = value;
+            arrayOfValues.push(keepValue);
+            arrayOfValues.push('/');
+            break;
+        case '=':
+            arrayOfValues.push(keepValue);
+            let evaluation = eval(arrayOfValues.join(' ')); //obliczamy wynik z połączonej tablicy
+            value = evaluation + '';
+            result.innerText = value;
+            arrayOfValues = []; //po podaniu wyniku czyścimy tablicę
+            prevResult.innerText = '';
+            break;
+        default:
+            break;
+    }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', showResult);
+}
+for (let i = 0; i < operations.length; i++) {
+    operations[i].addEventListener('click', calculations);
+}
+
+buttonReset.addEventListener('click', () => {
+    value = '';
+    prevResult.innerText = '';
+    keepValue = undefined;
+    arrayOfValues = [];
+    result.innerHTML = value;
+});
+
+buttonBackspace.addEventListener('click', () => {
+    let lengthOfvalue = value.length; //długość tablicy z przechowywanymi warościami zapisujemy do zmiennej
+    let lengthOfHistoryVal = prevResult.length
+    value = value.slice(0, lengthOfvalue - 1); //usuwamy ostatnią wprowadzoną cyfrę
+    result.innerText = value;
+    value = value.slice(0, lengthOfvalue - 1);
+    prevResult.innerText = value;
+    // prevResult.innerText += value; 
+});
+
+buttonPoint.addEventListener('click', () => {
+    if (!value.includes('.')) //jeżeli przechowywana wartość nie zawiera kropki
+        value += '.'; //dodaj kropkę
+        result.innerText = value; // wyświetlany wynik razem z kropką
+});
